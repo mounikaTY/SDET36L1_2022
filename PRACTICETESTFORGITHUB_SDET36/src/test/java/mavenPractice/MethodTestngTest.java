633925c10aef1a31f099f348bed3a95023c24234
Admin <Admin@DESKTOP-MNBM418>
2022-07-21 23:48:42 +0530

    second commit

--- PRACTICETESTFORGITHUB_SDET36/src/test/java/mavenPractice/MethodTestngTest.java
+++ PRACTICETESTFORGITHUB_SDET36/src/test/java/mavenPractice/MethodTestngTest.java
@@ -8,6 +8,7 @@ public class MethodTestngTest {
 	@Test
 	public void methodsPractice() {
 	Reporter.log("this is another class for testng",true);
+	//this is the first change
 	}
 	
 	public void practiceMethods() {
